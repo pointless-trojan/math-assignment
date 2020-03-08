48a60a32f51cacf36976060363b447530dd6765d
pointless-trojan <akhilesh.a.dubey@gmail.com>
2020-03-08 18:40:01 +0000

    Simple program that returns a printable list of Fizz Buzz numbers for passed range

--- src/main/java/com/akhi/math/mathassignment/NumberManipulationService.java
+++ src/main/java/com/akhi/math/mathassignment/NumberManipulationService.java
@@ -27,6 +27,20 @@ public class NumberManipulationService {
      * @return
      */
     public String convertMultiples(int lowerRange, int upperRange) {
+        return convertMultiples(lowerRange, upperRange, false);
+    }
+
+    /**
+     * Common method that takes care of both aspects of Fizz Buzz based on
+     * a discriminator includeContains
+     *
+     * @param lowerRange
+     * @param upperRange
+     * @param includeContains
+     * @return
+     */
+    public String convertMultiples(int lowerRange, int upperRange, boolean includeContains) {
+
         if (lowerRange <= 0 || upperRange <= 0) {
             throw new IllegalArgumentException("Negative numbers or zero not allowed");
         }
@@ -34,8 +48,44 @@ public class NumberManipulationService {
             throw new IllegalArgumentException(String.format("Lower value %d, is less that upper value %d", lowerRange, upperRange));
         }
         return IntStream.rangeClosed(lowerRange, upperRange)
-                .mapToObj(x -> x%3 == 0 && x%5 == 0? "FizzBuzz": x%3 == 0? "Fizz": x%5 == 0? "Buzz": ""+x) //TODO - possibly extract in a rule engine if we need additional flexibility and/or there are repetitive use cases
+                .mapToObj(x -> fizzCheck(x, includeContains) && buzzCheck(x, includeContains)? "FizzBuzz":
+                        fizzCheck(x, includeContains)? "Fizz":
+                                buzzCheck(x, includeContains)? "Buzz": ""+x) //TODO - possibly extract in a rule engine if we need additional flexibility and/or there are repetitive use cases
                 .collect(Collectors.joining(System.lineSeparator()));
 
+    }
+
+    /**
+     * Checks for valid Fizz clause based on passed value and contains clause
+     *
+     * @param x
+     * @param includeContains
+     * @return
+     */
+    private boolean fizzCheck(int x, boolean includeContains) {
+        return x%3 == 0 || (includeContains && (""+ x).contains("3"));
+    }
+
+    /**
+     * Checks for valid Fizz clause based on passed value and contains clause
+     *
+     * @param x
+     * @param includeContains
+     * @return
+     */
+    private boolean buzzCheck(int x, boolean includeContains) {
+        return x%5 == 0 || (includeContains && (""+ x).contains("5"));
+    }
+
+    /**
+     * Works similar to the convertMultiples class, with the variance being that it also checks for
+     * occurance of 3 or 5 as Fizz and Buzz respectively too
+     *
+     * @param lowerRange
+     * @param upperRange
+     * @return
+     */
+    public String convertMultiplesAndContainingNumbers(int lowerRange, int upperRange) {
+        return convertMultiples(lowerRange, upperRange, true);
     }
 }
